fb4bfaf0693affe86f63c84713f6306896d18877
kymec <kummaz@ukr.net>
2019-04-26 19:11:39 +0300

    ready

--- script.js
+++ script.js
@@ -16,20 +16,24 @@ console.log(date3.toLocaleString('ru', {weekday: 'long'}));
 
 //4) Задание
 var birthday = prompt('Введите дату своего рождения в формате 1990-10-01');
-var arrDate = birthday.split('-');
-var date4 = new Date();
-var birthDate = new Date(date4.getFullYear(), arrDate[1] - 1, arrDate[2]);
-var daysLeft = Math.floor((birthDate - date4) / (1000 * 60 * 60 * 24) + 1);
-var result;
-if (daysLeft < 0 && date4.getFullYear() % 4 == 3){ //если день рождения в следующем году и следующий год высокосный
-    result = daysLeft + 366;
-}else if(daysLeft < 0 && date4.getFullYear() % 4 != 3){ //если день рождения в следующем году и следующий год не высокосный
-    result = daysLeft + 365;
-} else {
-    result = daysLeft;
-}
-if(result){
-    alert('До дня рождения осталось ' + result + ' дней');
-} else {
+if (birthday != null){
+    var arrDate = birthday.split('-');
+    var date4 = new Date();
+    var birthDate = new Date(date4.getFullYear(), arrDate[1] - 1, arrDate[2]);
+    var daysLeft = Math.floor((birthDate - date4) / (1000 * 60 * 60 * 24) + 1);
+    var result;
+    if (daysLeft < 0 && date4.getFullYear() % 4 == 3){ //если день рождения в следующем году и следующий год высокосный
+        result = daysLeft + 366;
+    }else if(daysLeft < 0 && date4.getFullYear() % 4 != 3){ //если день рождения в следующем году и следующий год не высокосный
+        result = daysLeft + 365;
+    } else {
+        result = daysLeft;
+    }
+    if(result){
+        alert('До дня рождения осталось ' + result + ' дней');
+    } else {
+        alert('Не правильно введена дата');
+    }
+} else{
     alert('Не правильно введена дата');
 }
